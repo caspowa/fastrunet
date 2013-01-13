10a76f4f2d3fee3a834b56526cffb43ab43dd1a5
pavel-paulau <pavel.paulau@gmail.com>
2013-01-13 21:53:22 +0300

    implement website title and url generator generator

--- src/alexa_parser.py
+++ src/alexa_parser.py
@@ -13,3 +13,12 @@ class AlexaParser(object):
             for li in ul.xpath("li"):
                 for a in li.xpath("a"):
                     yield a.text.strip()
+
+    def get_websites(self, category):
+        url = self.CATEGORY_URL + "/Top/" + category.replace(" ", "_")
+        html = requests.get(url).text
+        tree = etree.HTML(html)
+        for li in tree.xpath("//*[@id=\"topsites-category\"]/ul/li"):
+            title = li.xpath("div[2]/h2/a")[0].text.strip()
+            link = li.xpath("div[2]/span")[0].text.strip()
+            yield title, link
