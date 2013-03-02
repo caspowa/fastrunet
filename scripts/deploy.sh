406620514f4e468ea3d7e359eab3ea3e8e8983f2
pavel-paulau <pavel.paulau@gmail.com>
2013-03-02 21:00:17 +0300

    update deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 virtualenv env
 env/bin/pip install --timeout=120 -r requirements.txt
 env/bin/uwsgi --stop /tmp/uwsgi.bportal.pid
-env/bin/python scripts/fill_secret_keys.py src/secret_keys.py /var/bportal.keys
+env/bin/python scripts/fill_secret_keys.py src/secret_keys.py
 env/bin/uwsgi -x uwsgi.xml
