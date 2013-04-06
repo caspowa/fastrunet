aecccdacc44559a4f2adda6bfc86580557a219cb
pavel-paulau <pavel.paulau@gmail.com>
2013-04-06 14:48:26 +0300

    use /dev/null instead of /dev/null

--- src/crawler.py
+++ src/crawler.py
@@ -89,8 +89,8 @@ class Daemon(object):
 
     def __init__(self):
         self.stdin_path = "/dev/null"
-        self.stdout_path = "/dev/tty"
-        self.stderr_path = "/dev/tty"
+        self.stdout_path = "/dev/null"
+        self.stderr_path = "/dev/null"
         self.pidfile_path = "/tmp/crawler.pid"
         self.pidfile_timeout = 5
 
