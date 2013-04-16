18e9a505a4d89aa0c769e6a0ba95db00129dead4
pavel-paulau <pavel.paulau@gmail.com>
2013-04-16 11:24:38 +0300

    check if webapp is running before stopping it

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -ex
 virtualenv env
 env/bin/pip install --timeout=120 -r requirements.txt
-env/bin/uwsgi --stop /tmp/uwsgi.bportal.pid
+
+if [ ! -f /tmp/bportal.pid ] && [ kill -0 `cat /tmp/uwsgi.bportal.pid` ]; then
+    env/bin/uwsgi --stop /tmp/uwsgi.bportal.pid
+fi
+
 env/bin/python scripts/fill_secret_keys.py src/secret_keys.py
+
 env/bin/uwsgi -x uwsgi.xml
+
 env/bin/python src/crawler.py stop
 env/bin/python src/crawler.py start
