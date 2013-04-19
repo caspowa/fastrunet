277054c0a5027c67e44a8ae71f22b00290281699
pavel-paulau <pavel.paulau@gmail.com>
2013-04-19 16:17:32 +0300

    define remote mongodb instance

--- src/settings.py
+++ src/settings.py
@@ -4,5 +4,8 @@ from secret_keys import HIDDEN_SECRET_KEY
 DEBUG = True
 
 MONGODB_DB = 'bportal'
+MONGODB_HOST = 'db2.caspowa.com'
+MONGODB_USERNAME = 'caspowa'
+MONGODB_PASSWORD = 'Udn(1sd%asO2fv^asd(2,cc_asd'
 
 SECRET_KEY = HIDDEN_SECRET_KEY
