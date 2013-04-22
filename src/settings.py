978595aec6fc15939fe85f493383a1bfc813ea0d
pavel-paulau <pavel.paulau@gmail.com>
2013-04-22 21:16:34 +0300

    use replica set for mongodb
    Change-Id: Icc2733f33f388b561e6e818c345b2891582750a4
    Reviewed-on: https://review.caspowa.com/212
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- src/settings.py
+++ src/settings.py
@@ -3,9 +3,7 @@ from secret_keys import HIDDEN_SECRET_KEY
 
 DEBUG = True
 
-MONGODB_DB = 'bportal'
-MONGODB_HOST = 'db2.caspowa.com'
-MONGODB_USERNAME = 'caspowa'
-MONGODB_PASSWORD = 'Udn1sd2asO2_fvasd2cc_asd'
+MONGODB_HOST = 'mongodb://caspowa:Udn1sd2asO2_fvasd2cc_asd@db1.caspowa.com,db2.caspowa.com/bportal?replicaSet=rs1'
+MONGODB_DB = True
 
 SECRET_KEY = HIDDEN_SECRET_KEY
